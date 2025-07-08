1453c7c14af10fc0c52174deecefb59427b91b59
Sahil Mir <mirsahilsahil1@gmail.com>
2025-07-08 15:45:02 +0530

    feat: pushed complete Echo AI project 🚀🔥

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDnfRjAbS5eH-16fe_5zvAONHWPGOmJsEg",
-  authDomain: "echo-ai-53d8b.firebaseapp.com",
-  projectId: "echo-ai-53d8b",
-  storageBucket: "echo-ai-53d8b.firebasestorage.app",
-  messagingSenderId: "494788972616",
-  appId: "1:494788972616:web:e28fab500417f30299887d",
-  measurementId: "G-5D739251QE"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId : process.env.REACT_APP_FIREBASE_MEASUREMENTID
 };
 
 const app = initializeApp(firebaseConfig);
